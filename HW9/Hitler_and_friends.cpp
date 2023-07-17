1ea3662a30c6df09b7e59f1fd8d14de4d1c7f4ef
Navid <navid8020@gmail.com>
2023-07-17 10:22:48 +0330

    HW9-Graph(III)-Hitler and Friends

---
+++ HW9/Hitler_and_friends.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+typedef long long ll;
+
+typedef pair<double, double> pi;
+
+vector<vector<pi>> graph;
+
+vector<double> dijkstra(ll start, ll n) {
+    vector<double> dist(n, INT_MAX);
+    dist[start] = 0;
+    priority_queue<pi, vector<pi>, greater<pi>> pq;
+    pq.push(make_pair(start, 0));
+
+    while (!pq.empty()) {
+        ll u = pq.top().first;
+        pq.pop();
+
+        for (ll i = 0; i < graph[u].size(); i++) {
+            double v = graph[u][i].first;
+            double w = graph[u][i].second;
+
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                pq.push(make_pair(v, dist[v]));
+            }
+        }
+    }
+
+    return dist;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+
+    string s; cin >> s;
+    graph.resize(10+n);
+    ll start = 0;
+    for (ll i = 0; i < s.length(); i++) {
+        if (i+1 < s.length())
+        {
+            graph[i].push_back(make_pair(i+1, 1));
+        }
+        if(i-1 >= 0)
+        {
+            graph[i].push_back(make_pair(i-1, 1));
+        }
+        graph[i].push_back(make_pair(s[i]-'0'+n, 0.5));
+        graph[s[i]-'0'+n].push_back(make_pair(i, 0.5));
+    }
+
+    vector<double> dist = dijkstra(start, n+10);
+    // for (ll i = 0; i < 10; i++) {
+    //     if (dist[i] == ll_MAX) {
+    //         cout << "INF ";
+    //     } else { 
+    //         cout << dist[i] << " ";
+    //     }
+    // }
+    cout << dist[n-1] << endl;
+
+    return 0;
+}
